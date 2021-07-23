d14a2436f49c820a672990fcd5f996caf63cce63
Kumari Priya <58588135+priya-github12@users.noreply.github.com>
2021-07-23 19:23:42 +0530

    Update ApprovalSection.js

--- src/ApprovalSection.js
+++ src/ApprovalSection.js
@@ -1,1 +1,43 @@
 
+:root {
+  --black-2: 
+rgba(0, 0, 0, 0.451);
+  --black: 
+rgba(0, 0, 0, 1);
+  --lucky-point: 
+rgba(23, 21, 99, 1);
+  --eclipse: 
+rgba(44, 24, 24, 0.588);
+  --victoria: 
+rgba(78, 85, 143, 1);
+  --mist-gray: 
+rgba(196, 196, 196, 1);
+  --mercury: 
+rgba(229, 229, 229, 1);
+  --white: 
+rgba(255, 255, 255, 1);
+
+  --font-size-xs: 11px;
+  --font-size-s: 12px;
+  --font-size-s2: 14px;
+  --font-size-m2: 18px;
+  --font-size-l2: 24px;
+
+  --font-family-prompt: "Prompt";
+  --font-family-proza_libre: "Proza Libre";
+  --font-family-roboto: "Roboto";
+  --font-family-roboto-light: "Roboto-Light";
+  --font-family-roboto-medium: "Roboto-Medium";
+  --font-family-roboto-regular: "Roboto-Regular";
+}
+function App() {
+  return <div className="rectangle-33"></div>;
+}
+
+export default App;
+
+function App() {
+  return <div className="rectangle-33"></div>;
+}
+
+
