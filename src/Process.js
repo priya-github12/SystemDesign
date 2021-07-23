fe017b230b9df46121558446718d128cd1b308d2
Kumari Priya <58588135+priya-github12@users.noreply.github.com>
2021-07-23 20:01:43 +0530

    Update Process.js

--- src/Process.js
+++ src/Process.js
@@ -1,1 +1,56 @@
+function App() {
+  return <div className="brd-1roboto-bold-white-18px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BRD</div>;
+}
+function App() {
+  return <div className="impact-analysisroboto-bold-white-18px">Impact Analysis</div>;
+}
+function App() {
+  return <div className="cmr-1roboto-bold-white-18px">CMR</div>;
+}
 
+.brd-1 {
+  letter-spacing: 0;
+  min-height: 27px;
+  width: 132px;
+}
+.cmr-1 {
+  letter-spacing: 0;
+  min-height: 32px;
+  width: 146px;
+}
+.test-plan {
+  letter-spacing: 0;
+  min-height: 32px;
+  width: 168px;
+}
+.development-1 {
+  letter-spacing: 0;
+  min-height: 19px;
+  width: 153px;
+}
+.code-review {
+  letter-spacing: 0;
+  min-height: 34px;
+  width: 149px;
+}
+.system-test {
+  letter-spacing: 0;
+  min-height: 37px;
+  width: 157px;
+}
+
+.performance-test {
+  letter-spacing: 0;
+  min-height: 25px;
+  width: 157px;
+}
+.user-acceptance {
+  letter-spacing: 0;
+  min-height: 27px;
+  width: 155px;
+}
+.approvals-1 {
+  letter-spacing: 0;
+  min-height: 20px;
+  width: 115px;
+}
