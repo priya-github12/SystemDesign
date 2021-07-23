c48cb96d3a227039840df9541e3ce7261698d3ac
Kumari Priya <58588135+priya-github12@users.noreply.github.com>
2021-07-23 19:19:45 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,1 +1,198 @@
+function App() {
+  return <p className="fill-up-the-form">&nbsp;&nbsp;Fill up the form:</p>;
+}
+function App() {
+  return <Text1>System/Project Name :</Text1>;
+}
+function App() {
+  return <div className="rectangle-31"></div>;
+}
+function App() {
+return <div className="rectangle-38"></div>;
+}
+
+.rectangle-38 {
+  background-color: var(--mist-gray);
+  height: 42px;
+  width: 200px;
+}
+
+.text-1 {
+  letter-spacing: 0;
+  min-height: 26px;
+  min-width: 233px;
+  width: 233px;
+}
+
+export default App;
+.fill-up-the-form {
+  color: var(--black);
+  font-family: var(--font-family-prompt);
+  font-size: var(--font-size-s2);
+  font-weight: 600;
+  letter-spacing: 0;
+  min-height: 24px;
+  width: 191px;
+}
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="desktop-1">
+      <div className="overlap-group2">
+        <div className="rectangle-2"></div>
+        <div className="log-outroboto-bold-white-18px">Log Out</div>
+        <img className="rectangle-24" src="" />
+      </div>
+      <div className="overlap-group1">
+        <div className="rectangle-7"></div>
+        <div className="rectangle-8"></div>
+        <div className="rectangle-9"></div>
+        <div className="rectangle-10"></div>
+        <div className="rectangle-11"></div>
+        <div className="rectangle-12"></div>
+        <div className="rectangle-13"></div>
+        <div className="rectangle-14"></div>
+        <div className="rectangle-15"></div>
+        <div className="rectangle-16"></div>
+        <div className="rectangle-17"></div>
+        <div className="brdroboto-bold-white-18px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BRD</div>
+        <div className="cmrroboto-bold-white-18px">CMR</div>
+        <div className="specsroboto-bold-white-18px">&nbsp;&nbsp;&nbsp;&nbsp; Specs</div>
+        <div className="test-planroboto-bold-white-18px">&nbsp;&nbsp; Test Plan</div>
+        <div className="developmentroboto-bold-white-18px">&nbsp;&nbsp; Development</div>
+        <div className="code-reviewroboto-bold-white-18px">&nbsp;&nbsp;Code Review</div>
+        <div className="impact-analysisroboto-bold-white-18px">Impact Analysis</div>
+        <div className="system-testroboto-bold-white-18px">System Test</div>
+        <div className="performance-testroboto-bold-white-18px">Performance Test</div>
+        <div className="user-acceptanceroboto-bold-white-18px">User Acceptance</div>
+        <div className="approvalsroboto-bold-white-18px">Approvals</div>
+        <img className="polygon-1" src="" />
+        <img className="polygon-2" src="" />
+        <img className="polygon-3" src="" />
+        <img className="polygon-4" src="" />
+        <img className="polygon-5" src="" />
+        <img className="polygon-9" src="" />
+        <img className="polygon-8" src="" />
+        <img className="polygon-10" src="" />
+      </div>
+      <div className="overlap-group">
+        <div className="rectangle-31"></div>
+        <div className="rectangle-33"></div>
+        <div className="rectangle-34"></div>
+        <div className="rectangle-68"></div>
+        <div className="rectangle-69"></div>
+        <div className="rectangle-70"></div>
+        <div className="rectangle-35"></div>
+        <div className="brd-1prompt-normal-white-18px">BRD</div>
+        <div className="brd-requestprompt-normal-white-18px">BRD Request</div>
+        <img className="polygon-11" src="" />
+        <div className="rectangle-38"></div>
+        <div className="rectangle-39"></div>
+        <div className="rectangle-43"></div>
+        <div className="rectangle-40"></div>
+        <div className="rectangle-42"></div>
+        <div className="rectangle-41"></div>
+        <div className="rectangle-44"></div>
+        <div className="rectangle-47"></div>
+        <div className="rectangle-48"></div>
+        <div className="rectangle-45"></div>
+        <p className="fill-up-the-form">&nbsp;&nbsp;Fill up the form:</p>
+        <Text1>System/Project Name :</Text1>
+        <Text1 className="requirement-title">Requirement Title :</Text1>
+        <Text1 className="text-2">Team From Business :</Text1>
+        <Text1 className="text-3">Chief of Business Area :</Text1>
+        <div className="placeprompt-normal-eclipse-14px">Name</div>
+        <div className="place-1prompt-normal-eclipse-14px">Name</div>
+        <div className="place-2prompt-normal-eclipse-14px">Name</div>
+        <div className="designationprompt-normal-black-14px-2">Designation</div>
+        <div className="designation-1prompt-normal-black-14px-2">Designation</div>
+        <img className="polygon-12" src="" />
+        <img className="polygon-22" src="" />
+        <img className="polygon-15" src="" />
+        <img className="polygon-13" src="" />
+        <img className="polygon-16" src="" />
+        <img className="polygon-18" src="" />
+        <img className="polygon-17" src="" />
+        <img className="polygon-14" src="" />
+        <Text1 className="requestor">Requestor :</Text1>
+        <Text1 className="text-4">Any Other Stackholders :</Text1>
+        <div className="text-5">*Max Limit 50 characters</div>
+        <Text1 className="text-6">Customer Desired Target Date :</Text1>
+        <div className="dd-mm-yyyy">DD-MM-YYYY</div>
+        <img className="rectangle-46" src="" />
+        <Text1 className="text-7">Impacted Business Area :</Text1>
+        <Text1 className="text-8">No. of Business Users Impacted :</Text1>
+        <div className="approxprompt-extra-light-black-12px">(Approx)</div>
+        <div className="number-3prompt-extra-light-black-12px">00</div>
+        <div className="rectangle-49"></div>
+        <div className="rectangle-50"></div>
+        <div className="rectangle-51"></div>
+        <div className="rectangle-52"></div>
+        <Text1 className="text-9">Business Case and Problem Statement</Text1>
+        <Text1 className="business-need">Business Need :</Text1>
+        <Text1 className="text-10">Objective to be achieved :</Text1>
+        <div className="rectangle-53"></div>
+        <div className="rectangle-54"></div>
+        <div className="rectangle-58"></div>
+        <div className="rectangle-56"></div>
+        <div className="text-11prompt-light-black-11px">*Max Limit 20 characters</div>
+        <img className="rectangle-55" src="" />
+        <img className="rectangle-57" src="" />
+        <img className="rectangle-59" src="" />
+        <div className="text-12prompt-light-black-11px">*Max Limit 100 characters</div>
+        <div className="text-13prompt-light-black-11px">*Max Limit 100 characters</div>
+        <div className="text-14prompt-light-black-11px">*Max Limit 100 characters</div>
+        <Text1 className="text-15">Scope (Functional and Locational)</Text1>
+        <Text1 className="text-16">Functional Scope of Requirement :</Text1>
+        <Text1 className="text-17">Locational Scope of Requirement :</Text1>
+        <Text1 className="type-of-change">Type of Change</Text1>
+        <div className="rectangle-60"></div>
+        <div className="rectangle-61"></div>
+        <div className="rectangle-62"></div>
+        <Text1 className="statutory">Statutory</Text1>
+        <Text1 className="enhancement">Enhancement</Text1>
+        <Text1 className="bug-resolution">Bug Resolution</Text1>
+        <img className="rectangle-63" src="" />
+        <img className="rectangle-64" src="" />
+        <img className="rectangle-65" src="" />
+        <div className="rectangle-66"></div>
+        <div className="rectangle-67"></div>
+        <img className="line-1" src="" />
+        <img className="line-2" src="" />
+        <img className="line-3" src="" />
+        <img className="line-4" src="" />
+        <Text1 className="sq-no">Sq. No.</Text1>
+        <Text1 className="kpi">KPI</Text1>
+        <Text1 className="uom">UOM</Text1>
+        <Text1 className="baseline">Baseline</Text1>
+        <Text1 className="target">Target</Text1>
+        <div className="select-reviewerprozalibre-medium-lucky-point-18px">Select Reviewer</div>
+        <div className="save-as-draftprozalibre-medium-lucky-point-18px">Save as Draft</div>
+        <img className="rectangle-71" src="" />
+        <div className="submitprozalibre-medium-lucky-point-18px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Submit</div>
+        <div className="signatureprozalibre-medium-lucky-point-18px">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Signature
+        </div>
+        <img className="rectangle-72" src="" />
+        <img className="polygon-23" src="" />
+        <img className="polygon-24" src="" />
+        <img className="polygon-25" src="" />
+        <Text1 className="number">0</Text1>
+        <Text1 className="number-1">0</Text1>
+        <Text1 className="number-2">0</Text1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+function Text1(props) {
+  const { children, className } = props;
+
+  return <p className={`text-1 prompt-normal-black-14px ${className || ""}`}>{children}</p>;
+}
+
 
