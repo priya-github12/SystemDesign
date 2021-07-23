8c3d61056a05dc7a5c87543acdea6dc43a0e4f5c
Kumari Priya <58588135+priya-github12@users.noreply.github.com>
2021-07-23 20:03:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function App() {
+  return <div className="log-outroboto-bold-white-18px">Log Out</div>;
+}
+function App() {
+  return <div className="specs-1roboto-bold-white-18px">&nbsp;&nbsp;&nbsp;&nbsp; Specs</div>;
+}
+function App() {
+  return <div className="text-1prompt-normal-white-18px">Impact Analysis Request</div>;
+}
+function App() {
+  return <div className="impact-analysisprompt-normal-white-18px">Impact Analysis</div>;
+}
 
+
+
+
+export default App;
